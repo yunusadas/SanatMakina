6fe1edae844e3805cda554ea53af9399bcfcd677
Yunus Emre Adaş <yunusemre.adas@stu.fsm.edu.tr>
2023-05-10 21:54:20 +0300

    Ana ürünlerin girilmesi

--- header.php
+++ header.php
@@ -102,12 +102,13 @@
                 <li>
                   <a href="projects">Ürünlerimiz</a>
                   <ul>
-                    <li><a href="blog-grid">Komple Hatlar</a></li>
                     <li><a href="ektam">Dolum Makineleri</a></li>
                     <li><a href="ektam">Şişeleme Makineleri</a></li>
                     <li><a href="ektam">Paletleme-Palet Bozma</a></li>
                     <li><a href="ektam">Etiketleme Makineleri</a></li>
-                    <li><a href="ektam">Pastörizasyon Hatları</a></li>
+                    <li><a href="pastorizasyon">Pastörizasyon Hatları</a></li>
+                    <li><a href="tanklar">Tanklar</a></li>
+                    <li><a href="shirink">Shrink Makineleri</a></li>
                   </ul>
                 </li>
               <li><a href="contact">İLETİŞİM</a></li>
---
+++ shirink.php
@@ -0,0 +1,91 @@
+<?php
+require_once 'header.php';
+?>
+    <section class="page-header padding">
+        <div class="container">
+            <div class="page-content text-center">
+                <h2>Shirink Makineleri</h2>
+                <p>1984'ten beri gururla</p>
+            </div>
+        </div>
+    </section>
+    <section class="projects-section padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6 padding-15">
+                    <div class="project-item">
+                        <img src="img/erciyes-front.jpg" alt="projects" />
+                        <div class="overlay"></div>
+                        <a href="shirink_detay" class="view-icon ajax-popup-link">
+                            <i class="fas fa-expand"></i></a>
+                        <div class="projects-content">
+                            <a href="shirink_detay" class="category">Sanat Makina</a>
+                            <h3>
+                                <a href="shirink_detay" class="tittle">Shirink Makinesi</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 padding-15">
+                    <div class="project-item">
+                        <img src="img/dimril_thumb.webp" alt="projects" />
+                        <div class="overlay"></div>
+                        <a href="shirink_detay" class="view-icon"> <i class="fas fa-expand"></i></a>
+                        <div class="projects-content">
+                            <a href="shirink_detay" class="category">Sanat Makina</a>
+                            <h3>
+                                <a href="shirink_detay" class="tittle">Dirmil Monoblok</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 padding-15">
+                    <div class="project-item">
+                        <img src="img/damla_thumb.webp" alt="projects" />
+                        <div class="overlay"></div>
+                        <a href="shirink_detay" class="view-icon"> <i class="fas fa-expand"></i></a>
+                        <div class="projects-content">
+                            <a href="shirink_detay" class="category">Sanat Makina</a>
+                            <h3>
+                                <a href="shirink_detay" class="tittle">Damla Temizleme</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="sponsor-section bg-grey">
+        <div class="dots"></div>
+        <div class="container">
+            <div id="sponsor-carousel" class="sponsor-carousel owl-carousel">
+                <div class="sponsor-item">
+                    <img src="img/sponsor1.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor2.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor3.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor4.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor5.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor6.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor7.png" alt="sponsor" />
+                </div>
+                <div class="sponsor-item">
+                    <img src="img/sponsor8.png" alt="sponsor" />
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+require_once 'footer.php';
+?>
